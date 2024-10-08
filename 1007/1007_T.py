42fbda9b38b0b0cc6f8ad98bb66d7887afc67a08
lazybear-0425 <wendy930425@gmail.com>
2024-10-08 12:39:29 +0800

    做完紙張跟T形

---
+++ 1007/1007_T.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+ 
+ 
+img = cv2.imread('T_pic.png', cv2.IMREAD_GRAYSCALE)
+canny = cv2.Canny(img, 5, 100, None, 3) # 輸出是二值圖
+dst = np.copy(canny)
+dst = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
+
+'''這邊要用Polistic的'''
+lines_P = cv2.HoughLinesP(canny, 1, np.pi / 180, 33, None, 50, 60)
+for line in lines_P:
+    line = line[0] # 因為 lines_P 的 shape 是 (9,1,4)
+    cv2.line(dst, (line[0], line[1]), (line[2], line[3]), (0, 0, 255), 3, cv2.LINE_AA)
+
+cv2.imshow('canny', canny)
+cv2.imshow('result', dst)
+cv2.waitKey(0)
+ 
---
+++ 1007/1007_word.py
@@ -0,0 +1,27 @@
+import cv2 as cv2
+import numpy as np
+from matplotlib import pyplot as plt
+ 
+img = cv2.imread('word.png', cv2.IMREAD_GRAYSCALE)
+
+threshold = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv2.THRESH_BINARY,3,2)
+cv2.imshow('threshold', threshold)
+
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 3))
+''' 因為是白底黑字，所以erode和dilate要相反操作 '''
+erode = cv2.erode(threshold, kernel)
+dilate = cv2.dilate(erode, kernel)
+cv2.imshow('open', dilate)
+''' 用膨脹去變成連線 '''
+kernel_dil = cv2.getStructuringElement(cv2.MORPH_RECT, (11, 1))
+erode_line = cv2.erode(dilate, kernel_dil)
+cv2.imshow('dilate', erode_line)
+
+cv2.waitKey(0) 
+
+'''
+第一張 二質化
+去雜訊
+連起來
+'''
